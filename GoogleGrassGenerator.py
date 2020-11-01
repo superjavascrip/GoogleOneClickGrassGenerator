f49157103498570abbe1fd38cf7b2e6d9ca3a270
superjavascrip <lronman1008000@gmail.com>
2020-11-01 17:27:03 +0800

    谷歌生草器 Version1.0

---
+++ GoogleGrassGenerator.py
@@ -0,0 +1,139 @@
+# coding:utf-8
+import random
+
+
+class GoogleGrassGenerator:
+    def __init__(self):
+        from googletrans import Translator
+        LANGUAGES = [
+            "af",
+            "sq",
+            "am",
+            "ar",
+            "hy",
+            "az",
+            "eu",
+            "be",
+            "bn",
+            "bs",
+            "bg",
+            "ca",
+            "ceb",
+            "ny",
+            "zh-cn",
+            "zh-tw",
+            "co",
+            "hr",
+            "cs",
+            "da",
+            "nl",
+            "en",
+            "eo",
+            "et",
+            "tl",
+            "fi",
+            "fr",
+            "fy",
+            "gl",
+            "ka",
+            "de",
+            "el",
+            "gu",
+            "ht",
+            "ha",
+            "haw",
+            "iw",
+            "hi",
+            "hmn",
+            "hu",
+            "is",
+            "ig",
+            "id",
+            "ga",
+            "it",
+            "ja",
+            "jw",
+            "kn",
+            "kk",
+            "km",
+            "ko",
+            "ku",
+            "ky",
+            "lo",
+            "la",
+            "lv",
+            "lt",
+            "lb",
+            "mk",
+            "mg",
+            "ms",
+            "ml",
+            "mt",
+            "mi",
+            "mr",
+            "mn",
+            "my",
+            "ne",
+            "no",
+            "ps",
+            "fa",
+            "pl",
+            "pt",
+            "pa",
+            "ro",
+            "ru",
+            "sm",
+            "gd",
+            "sr",
+            "st",
+            "sn",
+            "sd",
+            "si",
+            "sk",
+            "sl",
+            "so",
+            "es",
+            "su",
+            "sw",
+            "sv",
+            "tg",
+            "ta",
+            "te",
+            "th",
+            "tr",
+            "uk",
+            "ur",
+            "uz",
+            "vi",
+            "cy",
+            "xh",
+            "yi",
+            "yo",
+            "zu",
+            "he",
+        ]
+        self.LANGUAGES = LANGUAGES
+        self.translator = Translator(service_urls=[
+            'translate.google.cn'
+        ])
+        self.NumberOfLanguages = (len(self.LANGUAGES) - 1)
+
+    def getRandomGrass(self, OriginalText, frequency):
+        Text = OriginalText
+        list = []
+        for i in range(frequency):
+            if i != 0:
+                RandomLanguage = self.LANGUAGES[random.randint(0, self.NumberOfLanguages)]
+                list.append(RandomLanguage)
+                Text = self.translator.translate(Text, dest=RandomLanguage, src=list[i - 1]).text
+            elif i == 0:
+                list.append(self.translator.detect(Text).lang)
+                RandomLanguage = self.LANGUAGES[random.randint(0, self.NumberOfLanguages)]
+                Text = self.translator.translate(Text, dest=RandomLanguage,
+                                                 src=self.translator.detect(Text).lang).text
+        return self.translator.translate(Text, dest="zh-cn", src=list[frequency - 1]).text
+
+    def outputRandomGrassTxt(self, inputTxt, outputTxt, frequency):
+        inputText = open(inputTxt, "r", encoding="utf-8").read()
+        RandomGrass = self.getRandomGrass(inputText, frequency)
+        open(outputTxt, "w+", encoding="utf-8").write(RandomGrass)
---
+++ GrassGenerator.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+import os
+
+import GoogleGrassGenerator as GoogleGrassGenerator
+import argparse
+
+'''
+Copyright Github@superjavascrip Bilibili@兰格如同
+Version : 1.0
+'''
+GoogleGrassGenerator = GoogleGrassGenerator.GoogleGrassGenerator()
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", help="输入需要生草的txt文件(不输入进入交互翻译模式)", type=str)
+parser.add_argument("-o", help="输出的txt文件名(默认output.txt)", type=str, default="output.txt")
+parser.add_argument("-f", help="需要生草的次数(默认20次)", type=int, default=20)
+args = parser.parse_args()
+if args.i is None:
+    input_text = input("请输入需要生草的文字")
+    input_frequency = input("请输入需要生草的次数")
+    if input_text is None:
+        print("请输入文字！！！")
+    if input_frequency is None or input_frequency == "":
+        input_frequency = 20
+        if input_text is not None:
+            print(GoogleGrassGenerator.getRandomGrass(input_text, int(input_frequency)))
+elif args.i is not None:
+    if os.path.splitext(args.o)[-1] != "mp3":
+        GoogleGrassGenerator.outputRandomGrassTxt(args.i, args.o, args.f)
